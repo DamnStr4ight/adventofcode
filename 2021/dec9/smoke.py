c98668cfbcd1d72e0af74a956a0db45f94a3b051
DamnStr4ight <johan.vaarlid@gmail.com>
2021-12-10 01:19:49 +0100

    day 9 complete

--- 2021/dec9/smoke.py
+++ 2021/dec9/smoke.py
@@ -1,46 +1,20 @@
 map=[]
 low_points=[]
+unasigned_points=[]
 
 result1=0
 
-def basin_search(x,y):
-    point=map[y][x]
-    if point==9:
-        return 0
-    if x==0:
-        left=10
-    else:
-        left=map[y][x-1]
-    if x==x_max:
-        right=10
-    else:
-        right=map[y][x+1]
-    if y==0:
-        top=10
-    else:
-        top=map[y-1][x]
-    if y==y_max:
-        bottom=10
-    else:
-        bottom=map[y+1][x]            
-    #print('top: '+str(top)+' bottom: '+str(bottom)+' left: '+str(left)+' right: '+str(right))
-    if point<left or point<right or point<top or point<bottom:
-        
-    
-
-with open("example.txt") as data:
+with open("input.txt") as data:
     for line in data:
         map.append(list(line))
     for i in range(len(map)):
         map[i].pop()
         map[i] = [int(i) for i in map[i]]
-    #print(map)
-    #print('x elements '+str(len(map[0])))
-    #print('y elements '+str(len(map)))
     y_max=len(map)-1
     x_max=len(map[0])-1
     for y in range(len(map)):
         for x in range(len(map[0])):
+            unasigned_points.append([x,y])
             point=map[y][x]
             if x==0:
                 left=10
@@ -58,27 +32,34 @@ with open("example.txt") as data:
                 bottom=10
             else:
                 bottom=map[y+1][x]            
-            #print('top: '+str(top)+' bottom: '+str(bottom)+' left: '+str(left)+' right: '+str(right))
             if point<left and point<right and point<top and point<bottom:
                 result1+=point+1
                 low_points.append([x,y])
-    print(low_points)
-    basins=[[]for _ in range(len(low_points))]
-    print(basins)
+                unasigned_points.pop(-1)
+            if point==9:
+            #we dont want to assign 9s to any basin later
+                unasigned_points.pop(-1)
     print('Sum of risk level: '+str(result1))
+    
+    basins=[[]for _ in range(len(low_points))]
+    #solution 2
     for point in range(len(low_points)):
         basins[point].append(low_points[point])
-    for basin in range(len(basins)):
-        searching=1
-        while(searching):
-            #search top
-            success=basin_search(basins[basin][0]-1,basins[basin][1]-1)
-            if(success):
-                if [basins[basin][0]-1,basins[basin][1]-1] not in basins[basin]:
-                    basins[basin].append([basins[basin][0]-1,basins[basin][1]-1])
-            #check sides
-            #if side is lower than its sides, add the point to basin
-            
-        
-    
-            
+    while len(unasigned_points)>0:
+        for point in range(len(unasigned_points)):
+            for i in range(len(low_points)):
+                for basin_point in basins[i]:                              
+                    try:
+                        x_distance=abs(basin_point[0]-unasigned_points[point][0])
+                        y_distance=abs(basin_point[1]-unasigned_points[point][1])
+                        if x_distance==1 and y_distance==0 or x_distance==0 and y_distance==1:
+                            basins[i].append(unasigned_points[point])
+                            unasigned_points.pop(point)
+                    except:
+                        pass               
+    basin_sizes=[]
+    for basin in basins:
+        basin_sizes.append(len(basin))
+    basin_sizes.sort()
+    result2=basin_sizes[-1]*basin_sizes[-2]*basin_sizes[-3]
+    print('Sum of largest basins is: '+str(result2))
