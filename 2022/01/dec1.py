6e996fb605e03af975ad995ce241d67cc098e7e1
Johan Vaarlid <johan.vaarlid@eelume.com>
2022-12-01 15:28:36 +0100

    more cleanup

--- 2022/01/dec1.py
+++ 2022/01/dec1.py
@@ -1,18 +1,11 @@
-elves = [[]]
+
 idx=0
-with open("data.txt") as data:
-    for line in data:
-        if line == "\n":
-            idx+=1
-            elves.append([])
-        else:    
-            elves[idx].append(int(line))
+with open("example.txt") as data:
+    elves = [eval(x.split('\n')) for x in data.read().split("\n\n")]
+print(elves)
 max_cal=[0,0,0]
 for elf in elves:
-    curr_elf_cal = 0
-    for cal in elf:
-        curr_elf_cal +=cal
-    if sum(elf)>min(max_cal):
-        max_cal[max_cal.index(min(max_cal))]=sum(elf)
+    if sum(eval(x) for x in elf)>min(max_cal):
+        max_cal[max_cal.index(min(max_cal))]=sum(eval(x) for x in elf)
 print("Elf carrying the most: " + str(max(max_cal)))
 print("Top three elfs carries: " + str(sum(max_cal)))
