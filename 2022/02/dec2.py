49c29bac2a0c93ecfc7cab1e07f0ac9e43dc416e
Johan Vaarlid <johan.vaarlid@eelume.com>
2022-12-05 16:19:04 +0100

    updated day 2

--- 2022/02/dec2.py
+++ 2022/02/dec2.py
@@ -1,28 +1,6 @@
 
-outcomes ={
-       'A X': [3],
-       'A Y': [4],
-       'A Z': [9],
-       'B X': [1],
-       'B Y': [5],
-       'B Z': [9],
-       'C X': [7],
-       'C Y': [2],
-       'C Z': [6]
-      }
-
-
-outcomes2 ={
-       'A X': [3],
-       'A Y': [4],
-       'A Z': [8],
-       'B X': [1],
-       'B Y': [5],
-       'B Z': [9],
-       'C X': [2],
-       'C Y': [6],
-       'C Z': [7]
-      }
+outcomes ={'A X': [3], 'A Y': [4], 'A Z': [9], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [7], 'C Y': [2], 'C Z': [6]}
+outcomes2 ={'A X': [3], 'A Y': [4], 'A Z': [8], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [2], 'C Y': [6], 'C Z': [7]}
 my_points = 0
 secondstrat=0
 with open("data.txt",'r') as d:
