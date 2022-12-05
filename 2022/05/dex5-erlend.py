e68dc65228cd6db2717bb5ede66e62c94f44a6b6
DamnStr4ight <johan.vaarlid@gmail.com>
2022-12-05 23:04:25 +0100

    Update dex5-erlend.py

--- 2022/05/dex5-erlend.py
+++ 2022/05/dex5-erlend.py
@@ -3,7 +3,6 @@ lines = f.read().split('\n')
 stacks  = [[stack][0] for stack in [[line[i] for line in lines if len(line) > i and line[i].isupper()] for i in range(50)] if stack]
 stacks2 = [[stack][0] for stack in [[line[i] for line in lines if len(line) > i and line[i].isupper()] for i in range(50)] if stack]
 instructions = [[int(word) for word in line.split(' ') if word.isnumeric()] for line in lines if len(line) > 1 and line[0].islower()]
-print(instructions)
 for instruction in instructions:
     for i in range(instruction[0]):
         stacks[instruction[2]-1].insert(0, stacks[instruction[1]-1].pop(0))
