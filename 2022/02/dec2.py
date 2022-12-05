d756c51d31df0fa26aa6ead5d0f54ff719b3cccf
Johan Vaarlid <johan.vaarlid@eelume.com>
2022-12-05 16:20:01 +0100

    updated day 2

--- 2022/02/dec2.py
+++ 2022/02/dec2.py
@@ -1,11 +1,11 @@
 
-outcomes ={'A X': [3], 'A Y': [4], 'A Z': [9], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [7], 'C Y': [2], 'C Z': [6]}
-outcomes2 ={'A X': [3], 'A Y': [4], 'A Z': [8], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [2], 'C Y': [6], 'C Z': [7]}
-my_points = 0
-secondstrat=0
+outcomes    = {'A X': [3], 'A Y': [4], 'A Z': [9], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [7], 'C Y': [2], 'C Z': [6]}
+outcomes2   = {'A X': [3], 'A Y': [4], 'A Z': [8], 'B X': [1], 'B Y': [5], 'B Z': [9], 'C X': [2], 'C Y': [6], 'C Z': [7]}
+my_points   = 0
+secondstrat = 0
 with open("data.txt",'r') as d:
     for line in d:
-        my_points+=outcomes[line.strip("\n")][0]
-        secondstrat+=outcomes2[line.strip("\n")][0]
+        my_points   += outcomes[line.strip("\n")][0]
+        secondstrat += outcomes2[line.strip("\n")][0]
 print("My points first strat: "+ str(my_points))
 print("My points second strat: "+ str(secondstrat))
