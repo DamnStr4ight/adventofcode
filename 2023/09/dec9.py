8ac6ff92376d82505acb5fc5561d3dc739bfdd13
DamnStr4ight <johan.vaarlid@gmail.com>
2023-12-10 16:40:53 +0100

    Day 9 is improved with less for loops

--- 2023/09/dec9.py
+++ 2023/09/dec9.py
@@ -11,21 +11,15 @@ for h in histories:
         for n in range(len(h[i])-1):
             h[i+1].append(h[i][n+1]-h[i][n])
         i+=1
-for h in histories:
     for l in range(len(h)-1,-1,-1):
         if(h[l][0]==h[l][1]==h[l][-1]==0):
             h[l].append(0)
-        else:
-            h[l].append(h[l][-1]+h[l+1][-1])
-for h in histories:
-    for l in range(len(h)-1,-1,-1):
-        if(h[l][0]==h[l][1]==h[l][-1]==0):
             h[l].insert(0,0)
         else:
+            h[l].append(h[l][-1]+h[l+1][-1])
             h[l].insert(0,h[l][0]-h[l+1][0])
-
-for h in histories:
     sum+=h[0][-1]
-    sum2+=h[0][0]
+    sum2+=h[0][0]        
+    
 print("Answer1: " + str(sum))
 print("Answer2: " + str(sum2))
